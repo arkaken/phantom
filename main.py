83dbb8f8c9cae81963f8dec1b4ddf47f96fdc24c
arkaken <118191916+arkaken@users.noreply.github.com>
2024-02-24 11:37:31 +0700

    main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+from bip_utils import Bip39SeedGenerator, Bip44Coins, Bip44, base58, Bip44Changes
+
+class BlockChainAccount():
+
+    def __init__(self, mnemonic, coin_type=Bip44Coins.ETHEREUM, password='') -> None:
+
+        self.mnemonic = mnemonic.strip()
+        self.coin_type = coin_type
+        self.password = password # if have password
+
+    def get_address_pk(self):
+
+        seed_bytes = Bip39SeedGenerator(self.mnemonic).Generate(self.password)
+        if self.coin_type != Bip44Coins.SOLANA:
+            bip44_mst_ctx = Bip44.FromSeed(seed_bytes, self.coin_type).DeriveDefaultPath()
+            return bip44_mst_ctx.PublicKey().ToAddress(), bip44_mst_ctx.PrivateKey().Raw().ToHex()
+        else:
+            bip44_mst_ctx = Bip44.FromSeed(seed_bytes, self.coin_type)
+           
+            bip44_acc_ctx = bip44_mst_ctx.Purpose().Coin().Account(0)
+            bip44_chg_ctx = bip44_acc_ctx.Change(Bip44Changes.CHAIN_EXT) # if you use "Solflare", remove this line and make a simple code modify and test
+            priv_key_bytes = bip44_chg_ctx.PrivateKey().Raw().ToBytes()
+            public_key_bytes = bip44_chg_ctx.PublicKey().RawCompressed().ToBytes()[1:]
+            key_pair = priv_key_bytes+public_key_bytes
+
+            return bip44_chg_ctx.PublicKey().ToAddress(), base58.Base58Encoder.Encode(key_pair)
